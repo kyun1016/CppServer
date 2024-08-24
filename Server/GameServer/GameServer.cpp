27bece3298e7567cc089c6c281b653db4d736c5b
kyun1016 <42373004+kyun1016@users.noreply.github.com>
2024-08-25 03:02:28 +0900

    Implemented and utilized thread.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -2,12 +2,46 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HellowThread()
+{
+    cout << "Hello Thread" << endl;
+}
+void HellowThread2(int32 num)
+{
+    cout << num << endl;
+}
+
+// t.get_id();                  // Thread ID
+// t.hardware_concurrency();    // CPU 코어 개수
+// t.detach();                  // Thread 객체에서 실제 쓰레드 분리
+// t.joinable())                
+// t.join();
+
 int main()
 {
     std::unique_ptr<ServerCore::CorePch> server;
     
     server = std::make_unique< ServerCore::CorePch>();
 
+    
+
+    std::vector<std::thread> v;
+
+    for (int32 i = 0; i < 10; ++i)
+    {
+        v.push_back(std::thread(HellowThread2, i));
+    }
+
     server->HelloWorld();
 
+    for (int32 i = 0; i < 10; ++i)
+    {
+        if (v[i].joinable())
+            v[i].join();
+    }
+    
+
+    return 0;
 }
