1cc610c7e2ec42c733638efb3ee14a8cf8a4fa02
kyun1016 <42373004+kyun1016@users.noreply.github.com>
2024-08-31 19:48:02 +0900

    Implement logic to replace the constructors of data containers included in the std library with a custom-made allocator.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -30,60 +30,23 @@ public:
 		cout << "~Knight()" << endl;
 	}
 
-	//static void* operator new(size_t size)
-	//{
-	//    cout << "Knight new! " << size << endl;
-	//    void* ptr = ::malloc(size);
-	//    return ptr;
-	//}
-
-	//static void operator delete(void* ptr)
-	//{
-	//    cout << "Knight delete!" << endl;
-	//    ::free(ptr);
-	//}
-
 	int64 _hp = 10;
 	int32 _damage = 100;
 	int32 _damage2 = 100;
 	int32 _damage3 = 100;
 };
 
-//// new operator overloding (Global)
-//void* operator new(size_t size)
-//{
-//	cout << "new! " << size << endl;
-//	void* ptr = ::malloc(size);
-//	return ptr;
-//}
-//
-//
-//void operator delete(void* ptr)
-//{
-//	cout << "delete!" << endl;
-//	::free(ptr);
-//}
-//
-//void operator delete[](void* ptr)
-//{
-//	cout << "delete[]!" << endl;
-//	::free(ptr);
-//}
-//
-//void* operator new[](size_t size)
-//{
-//	cout << "new[]!" << size << endl;
-//	void* ptr = ::malloc(size);
-//	return ptr;
-//}
-
-
-
 int main()
 {
-	Knight* knight = (Knight*) xnew<Player>();
-	knight->_hp = 100;
-	xdelete(knight);
-	knight->_hp = 100;
+	cout << "vector" << endl;
+	Vector<Knight> v(10);
+	cout << "list" << endl;
+	List<Knight> l(10);
+
+	cout << "map" << endl;
+	Map<int32, Knight> m;
+	m.insert(std::make_pair<int32, Knight>(0, Knight()));
+	cout << m[0]._hp << endl;
+
 	return 0;
 }
