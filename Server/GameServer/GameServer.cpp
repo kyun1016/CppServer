b9baf021dc235173292abf3b2e1d2b29634cc9ac
kyun1016 <42373004+kyun1016@users.noreply.github.com>
2024-08-31 12:30:10 +0900

    Implement an algorithm to find prime numbers using multithreading.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -8,31 +8,63 @@
 #include <Windows.h>
 #include "ThreadManager.h"
 
-#include "PlayerManager.h"
-#include "AccountManager.h"
+bool IsPrime(int number)
+{
+    if (number <= 1)
+        return false;
+    if (number == 2 || number == 3)
+        return true;
+
+    for (int i = 2; i < number; ++i)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// [start, end]
+int CountPrime(int start, int end)
+{
+    int count = 0;
+    for (int number = start; number <= end; ++number)
+    {
+        if (IsPrime(number))
+            ++count;
+    }
+
+    return count;
+}
 
 int main()
 {
+    // 1000 = 168
+    // 1'0000 = 1229
+    // 100'0000 = 78498
+    const int MAX_NUMBER = 100'0000;
+    int coreCount = thread::hardware_concurrency();
+    int jobCount = (MAX_NUMBER / coreCount) + 1;
 
-    GThreadManager->Launch([=]
-        {
-            while (true)
-            {
-                cout << "Player Then Account" << endl;
-                GPlayerManager.PlayerThenAccount();
-                this_thread::sleep_for(100ms);
-            }
-        });
+    vector<thread> threads;
+    threads.reserve(coreCount);
+    
+    atomic<int> primeCount = 0;
+    for (int i = 0; i < coreCount; ++i)
+    {
+        int start = (i * jobCount) + 1;
+        int end = min(((i + 1) * jobCount), MAX_NUMBER);
 
-    GThreadManager->Launch([=]
-        {
-            while (true)
+        threads.push_back(thread([start, end, &primeCount]
             {
-                cout << "Account Then Player" << endl;
-                GAccountManager.AccountThenPlayer();
-                this_thread::sleep_for(100ms);
-            }
-        });
+                primeCount += CountPrime(start, end);
+            }));
+    }
+
+    for (thread& t : threads)
+        t.join();
+
+    cout << primeCount << endl;
+
 
     GThreadManager->Join();
     return 0;
