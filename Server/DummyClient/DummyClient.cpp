bfa947b7eb8a8e74175519db36c9f2b59574f66b
kyun1016 <42373004+kyun1016@users.noreply.github.com>
2024-09-01 14:40:00 +0900

    Implement a connected UDP server.

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -18,15 +18,11 @@ int main()
 	// 소켓 생성
 
 	// 윈속 초기화 (ws2_32 라이브러리 초기화)
-	// 관련 정보가 wsaData에 채워짐
 	WSADATA wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return -1;   // 시작이 안되는 경우 오류로 
 
-	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
 	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
-	// protocol : 0
-	// return : descriptor
 	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
@@ -41,25 +37,26 @@ int main()
 	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
 
-
+	// Connected UDP
+	::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
 
 	while (true)
 	{
-		// ---------------------------
-		// 송신
-		for (int i = 0; i < 10; ++i) {
-			char sendBuffer[100] = "Hello World";
+		char sendBuffer[100] = "Hello World";
 
-			// 자동으로 나의 IP 주소 + 포트 번호 설정
-			int32 resultCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-			if (resultCode == SOCKET_ERROR)
-			{
-				HandleError("Send To");
-				return 0;
-			}
+		// 자동으로 나의 IP 주소 + 포트 번호 설정
+		//// Unconnected UDP
+		//int32 resultCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
 
-			cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
+		// Connected UDP
+		int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+		if (resultCode == SOCKET_ERROR)
+		{
+			HandleError("Send To");
+			return 0;
 		}
+
+		cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
 
 
 		// ---------------------------
@@ -70,7 +67,10 @@ int main()
 
 		char recvBuffer[1000];
 
-		int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&recvAddr, &addrLen);
+		// // Unconnected UDP
+		// int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&recvAddr, &addrLen);
+		// Connected UDP
+		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
 		if (recvLen <= 0)
 		{
 			HandleError("RecvFrom");		// TODO: 추후 멀티쓰레드에서는 오류가 발생한 통신만 끊어주는 방식으로 변경 필요
